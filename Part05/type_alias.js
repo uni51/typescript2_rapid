1e189432e46e3c94a828bdea6b0cbb1cfab97dc1
uni51 <uni51@users.noreply.github.com>
2020-06-19 09:54:22 +0900

    5-4 型エイリアス

---
+++ Part05/type_alias.js
@@ -0,0 +1,2 @@
+// FooType型の変数dataを定義
+var data = ['abc', 'xyz', true];
---
+++ Part05/type_alias.ts
@@ -0,0 +1,4 @@
+// タプル型にFooTypeという名前を付与
+type FooType = [ string, number, boolean ];
+// FooType型の変数dataを定義
+let data: FooType = ['abc', 'xyz', true];
