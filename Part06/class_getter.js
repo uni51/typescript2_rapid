50ee5bbb8307c555a3465ceb6835ceb5eb3b4387
uni51 <uni51@users.noreply.github.com>
2020-06-29 13:23:45 +0900

    6-4 getter/setterアクセサー（修正）

--- Part06/class_getter.js
+++ Part06/class_getter.js
@@ -1,22 +1,23 @@
-var Person = /** @class */ (function () {
-    function Person() {
-    }
-    Object.defineProperty(Person.prototype, "age", {
-        // getterアクセサー
-        get: function () {
-            return this._age;
-        },
-        set: function (value) {
-            if (value < 0) {
-                throw new RangeError('ageプロパティは正数で指定してください。');
-            }
-            this._age = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return Person;
-}());
-var p = new Person();
-p.age = 10;
-console.log(p.age);
+{
+    var Person = /** @class */ (function () {
+        function Person() {
+        }
+        Object.defineProperty(Person.prototype, "age", {
+            get: function () {
+                return this._age;
+            },
+            set: function (value) {
+                if (value < 0) {
+                    throw new RangeError('ageプロパティは正数で指定してください。');
+                }
+                this._age = value;
+            },
+            enumerable: false,
+            configurable: true
+        });
+        return Person;
+    }());
+    var p = new Person();
+    p.age = 10;
+    console.log(p.age);
+}
--- Part06/class_getter.ts
+++ Part06/class_getter.ts
@@ -1,18 +1,18 @@
-class Person {
-  private _age!: number;
-  // getterアクセサー
-  get age(): number {
-    return this._age;
-  }
-
-  set age(value: number) {
-    if(value < 0) {
-      throw new RangeError('ageプロパティは正数で指定してください。');
+{
+  class Person {
+    private _age!: number;
+    get age(): number {
+      return this._age; 
     }
-    this._age = value;
+    set age(value: number) {
+      if (value < 0) {
+        throw new RangeError('ageプロパティは正数で指定してください。');
+      }
+      this._age = value;
+    }
   }
-}
 
-let p = new Person();
-p.age = 10;
-console.log(p.age);
+  let p = new Person();
+  p.age = 10;
+  console.log(p.age);
+}
